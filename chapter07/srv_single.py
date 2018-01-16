cc42cb16cc8d5e02c05959ed809cff21f537d898
Zhong Xiao <729457563@qq.com>
2018-01-16 22:18:11 +0800

    add chapter7: 服务器架构

---
+++ chapter07/srv_single.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#coding=utf-8
+# 一个单线程服务器
+
+import zen_utils
+
+if __name__ == '__main__':
+	address = zen_utils.parse_command_line('simple single-threaded server')
+	listener = zen_utils.create_srv_socket(address)
+	zen_utils.accept_connections_forever(listener)
